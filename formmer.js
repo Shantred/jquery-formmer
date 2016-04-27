afae3cd1baace574ce5ffa2fe1757d9bc84438c1
Eric South <shantred@gmail.com>
2016-04-27 18:01:19 -0500

    snapshot of plugin state before moving all functions to plugin internals

--- formmer.js
+++ formmer.js
@@ -7,10 +7,6 @@
  **/
  
 ;(function( $, window, document, undefined ) {
- 
-    var loadingText = "Please Wait";
-    var formmerUrls = [];
-    var references = [];
 
     var pluginName = "formmer",
         defaults = {
@@ -18,7 +14,8 @@
           enableOnSucces: false,
           watchURL: null,
           enabledCallback: null,
-          disabledCallback: null
+          disabledCallback: null,
+          loadingHTML: "Please Wait"
         };
 
     function Plugin( element, options ) {
@@ -49,11 +46,7 @@
             return false;
           }
 
-          disableSubmission( form );
-
-          // Attempt to access plugin closure
-          references[0].testClosure2();
-          //console.log("What about this?", $(form));
+          this.disableSubmission();
           return false;
         });
       },
@@ -61,27 +54,82 @@
         $(form).addClass('formmer')
               .attr('data-enableonerror', this.settings.enableOnError)
               .attr('data-enableonsuccess', this.settings.enableOnSuccess)
-              .attr('data-formmerid', 1);
+              .attr('data-formmerid', ($("[data-formmerid]").length + 1));
 
         // If a watchURL was provided, add it to the urls to watch
         if( this.settings.watchURL ) {
-          formmerUrls.push( this.settings.watchURL );
+          //formmerUrls.push( this.settings.watchURL );
           $(form).attr('data-watchurl', this.settings.watchURL);
         }
       },
-      testClosure: function() {
-        console.log("testing closure here");
+      enableSubmission: function( error ) {
+        $(this.element).removeClass('formmer-processing');
+
+        var button = $(this.element).find('button[type=submit]');
+        if( button ) {
+
+          // Check correct data attr depending on error value
+          if( error ) {
+
+            // By default, buttons should enable after an error
+            if( $(this.element).attr('data-enableonerror') ) {
+              if( $(this.element).attr('data-enableonerror') == "true") {
+                this.enableButton( button );
+              }
+            } else {
+            
+              // Not specified, default to enabled
+              this.enableButton( button );
+            }
+          } else {
+
+            // By default, buttons don't enable after success
+            if( $(this.element).attr('data-enableonsuccess') ) {
+              if( $(this.element).attr('data-enableonsuccess') === "true" ) {
+                this.enableButton( button );
+              }
+            } // No else because of defaults
+          }
+        }
       },
-      testClosure2: function() {
-        console.log("Testing new closure");
+      enableButton: function( button ) {
+        console.log("here we are");
+        button.html( button.attr('data-originalhtml') )
+              .prop('disabled', false)
+              .removeAttr('data-originalhtml');
+
+        if( this.settings.enabledCallback ) {
+          this.settings.enabledCallback.call(this, this.element, button);
+        }
+      },
+      disableSubmission: function() {
+        $(this.element).addClass('formmer-processing');
+
+        var button = $(this.element).find('button[type=submit]');
+        if( button ) {
+          disableButton( button );
+        }
+      },
+      disableButton: function( button ) {
+        // If provided, replace the button text data-loadinghtml
+        if( button.attr('data-loadinghtml') ) {
+          button.attr('data-originalhtml', button.html())
+                .html( button.attr('data-loadinghtml') )
+                .prop('disabled', true);
+        } else {
+          button.attr('data-originalhtml', button.html())
+                .html(loadingText)
+                .prop('disabled', true);
+        }
       }
     });
 
     // A really lightweight plugin wrapper around the constructor, preventing 
     // against multiple instantiations
     $.fn[ pluginName ] = function( options ) {
-      references.push( new Plugin( this, options ) );
-      return references[0];
+      
+      $.data( document, pluginName + "-" + ($("[data-formmerid]").length + 1), new Plugin( this, options ) );
+      return $.data(document, pluginName + "-" + + ($("[data-formmerid]").length + 1));
       // console.log(this.each);
       // return this.each( function() {
       //   if( !$.data( this, "plugin_" + pluginName ) ) {
@@ -94,9 +142,9 @@
 
 
     // Automatically apply formmer to every form with class formmer
-    $.each( $('form.formmer'), function(idx, form) {
-      $(form).formmer();
-    });
+    // $.each( $('form.formmer'), function(idx, form) {
+    //   $(form).formmer();
+    // });
 
     var enableSubmission = function( form, error ) {
       form.removeClass('formmer-processing');
@@ -160,27 +208,35 @@
     // Ajax prefilter that attaches our button control functions to outgoing ajax
     // requests if they are submissions to known forms
     $.ajaxPrefilter( function( options, originalOptions, jqXHR) {
-      if( formmerUrls.indexOf(options.url) > -1 ) {
 
-        // Attempt to get a form with the watched url first. If this fails, get the
-        // form by it's data-watchurl property
-        var form = $("form.formmer[action='" + options.url + "']");
+      // Attempt to get a form with the watched url first. If this fails, get the
+      // form by it's data-watchurl property
+      var form = $("form.formmer[data-watchurl='" + options.url + "']");
 
-        if( form.length === 0 ) {
-          form = $("form.formmer[data-watchurl='" + options.url + "']");
-        }
+      if( form.length === 0 ) {
+        form = $("form.formmer[action='" + options.url + "']");
+      }
+
+      // If form was found, get plugin instance and attach success and error handlers
+      if( form.length ) {
+
+        var pluginInstance = $.data(document, pluginName + "-" + form.attr('data-formmerid'));
 
         // If a success function exists, we need to check if it is already an array
         // of functions or a single function
         if( options.hasOwnProperty('success') ) {
+          console.log("has success");
           if( options.success.constructor === Array ) {
+            console.log("adding to array");
             options.success.push( function() { 
-              enableSubmission(form, false);
-              
+              console.log("using instance");
+              pluginInstance.enableSubmission(false);
             });
           } else {
+            console.log("not adding to array", options);
+            console.log("the type is", options.success.constructor);
             options.success = [ options.success, function() {
-              enableSubmission(form, false);
+              pluginInstance.enableSubmission(false);
             }];
           }
         } else {
@@ -205,5 +261,7 @@
           }
         }
       }
+
+      
     });
 }( jQuery, window, document ));
